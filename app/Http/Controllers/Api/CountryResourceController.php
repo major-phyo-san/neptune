dd1c8d54034e59522cff9b4c787758cc37a19f65
major-phyo-san <major-phyo-san1995@users.noreply.github.com>
2019-06-19 14:24:37 +0630

    Update CountryResourceController.php

--- app/Http/Controllers/Api/CountryResourceController.php
+++ app/Http/Controllers/Api/CountryResourceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\CountryResource;
 use App\Models\Country;
 
 class CountryResourceController extends Controller
