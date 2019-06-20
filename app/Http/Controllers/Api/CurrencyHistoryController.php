6e94a593f325ec73dcfcf88f02c77eed245555b5
major-phyo-san <major-phyo-san1995@users.noreply.github.com>
2019-06-20 12:02:22 +0630

    Update CurrencyHistoryController.php

--- app/Http/Controllers/Api/CurrencyHistoryController.php
+++ app/Http/Controllers/Api/CurrencyHistoryController.php
@@ -60,72 +60,6 @@ class CurrencyHistoryController extends Controller
 
     public function show(Request $request, $currency_code)
     {
-<<<<<<< HEAD
-    	$currency_codes = ['USD'];
-    	if($request->has('codes'))
-    	{
-    		$currency_codes = explode(',', $request->input('codes'));
-    	}
-    	$arr_count = count($currency_codes);
-    	$date = '2019-01-01';
-    	if($request->has('date'))
-    	{
-    		$date = $request->input('date');
-    	}
-
-    	$response = [
-    		"success" => true,
-    		"timestamp" => time(),
-    		"date" => $date,
-    		"exchange_type" => "historical",
-    		"base" => "USD",    		
-    	];
-    	
-    	
-    	if($arr_count==1)
-    	{    		
-    		$country = Country::query()->where('currency_code','=',$currency_codes[0])->get();
-			$country_id = $country[0]->id;	
-	
-			$rate = Rate::query()->where('recorded_date','=',$date)->where('country_id','=',$country_id)->get();
-			$response += [
-			'currency' => [
-				'rate' => $rate[0]->currency_rate,
-				'name' => $country[0]->currency_name,
-				'code' => $country[0]->currency_code,
-				'symbol' => $country[0]->currency_symbol,
-				'country_code' => $country[0]->country_code,
-							],
-					];	
-    	}
-
-    	else
-    	{
-    		$contentCollection = [$arr_count];   
-			$country_id = [$arr_count];  
-
-    		for($i=0; $i<$arr_count; $i++)
-    		{  			  		
-    			$country = Country::query()->where('currency_code','=',$currency_codes[$i])->get(); 
-    			$country_id[$i] = $country[0]->id;
-    			$rate = Rate::query()->where('recorded_date','=',$date)->where('country_id','=',$country_id[$i])->get();
-
-    			$contentCollection[$i] = [
-    				"rate" => $rate[0]->currency_rate,
-    				'name' => $country[0]->currency_name,
-					'code' => $country[0]->currency_code,
-					'symbol' => $country[0]->currency_symbol,
-					'country_code' => $country[0]->country_code,
-    			];
-    		}
-
-    		$response += [
-    			"currencies" => $contentCollection,
-    			];
-    	}
-
-    	return response()->json($response)
-=======
         $successFlag = true;
         $date = '2019-01-01';
         if($request->has('date'))
@@ -156,7 +90,6 @@ class CurrencyHistoryController extends Controller
         ];  
 
         return response()->json($response)
->>>>>>> side-branch
          ->withHeaders([
             "Content-Type" => 'application/json; charset=utf-8',
             "Access-Control-Allow-Origin" => '*',
@@ -166,37 +99,6 @@ class CurrencyHistoryController extends Controller
 
     public function batch(Request $request)
     {
-<<<<<<< HEAD
-    	$date = '2019-01-01';
-    	if($request->has('date'))
-    	{
-    		$date = $request->input('date');
-    	}
-
-    	$response = [
-    		"success" => true,
-    		"timestamp" => time(),
-    		"date" => $date,
-    		"exchange_type" => "historical",
-    		"base" => "USD",    		
-    	];
-
-    	$country = Country::query()->where('currency_code','=',$currency_code)->get();
-		$country_id = $country[0]->id;	
-	
-		$rate = Rate::query()->where('recorded_date','=',$date)->where('country_id','=',$country_id)->get();
-
-		$response += [
-			'currency' => [
-				'rate' => $rate[0]->currency_rate,
-				'name' => $country[0]->currency_name,
-				'code' => $country[0]->currency_code,
-				'symbol' => $country[0]->currency_symbol,
-				'country_code' => $country[0]->country_code,
-							],
-					];	
-		return response()->json($response)
-=======
         $successFlag = true;
         $currency_codes = ['USD','MMK'];
         if($request->has('codes'))
@@ -243,7 +145,6 @@ class CurrencyHistoryController extends Controller
         ];
 
         return response()->json($response)
->>>>>>> side-branch
          ->withHeaders([
             "Content-Type" => 'application/json; charset=utf-8',
             "Access-Control-Allow-Origin" => '*',
